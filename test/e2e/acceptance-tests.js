78ced56582aeca9a09c13a3ab17cd9939524dad0
Gregory Haskins <greg@greghaskins.com>
2015-09-01 16:31:19 -0400

    add mechanism for running e2e tests

--- test/e2e/acceptance-tests.js
+++ test/e2e/acceptance-tests.js
@@ -3,10 +3,21 @@
 var expect = require('chai').expect;
 var fs = require('fs');
 
-describe('The Expo format examples', function(){
+describe('The Expo format spec', function(){
 
-	var specText = fs.readFileSync(__dirname + '/expo-format/spec.txt', 'utf8');
+	var specText = fs.readFileSync(__dirname + '/expo-format/spec.txt', 'utf8')
+	var regex = /\n```html\s*\n([\s\S]+)\n```\s*\n```json\s*\n([\s\S]+)\n```\s*\n/g
 
-	console.log(specText);
+	var match;
+	var index = 0;
+	while ((match = regex.exec(specText)) != null) {
+		index++
+		var inputHTML = match[1];
+		var expectedJSON = match[2];
+
+		xit('example ' + index, function(){
+			expect(inputHTML).to.equal(inputHTML);
+		});
+	}
 
 });
