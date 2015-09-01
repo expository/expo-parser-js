ed3c89f9f7f63a71cb40cccdf3146ee289d3771b
Gregory Haskins <greg@greghaskins.com>
2015-09-01 16:00:36 -0400

    add acceptance tests to browser suite

--- test/e2e/acceptance-tests.js
+++ test/e2e/acceptance-tests.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 
 describe('The Expo format examples', function(){
 
-	var specText = fs.readFileSync(__dirname + '/expo-format/spec.txt');
+	var specText = fs.readFileSync(__dirname + '/expo-format/spec.txt', 'utf8');
 
 	console.log(specText);
 
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -20,7 +20,12 @@ module.exports = function(config) {
       included: true,
       served: true
     }, {
-      pattern: 'src/**/.js',
+      pattern: 'test/e2e/*.js',
+      watched: true,
+      included: true,
+      served: true
+    }, {
+      pattern: 'src/**/*.js',
       watched: true,
       included: false,
       served: false
@@ -34,7 +39,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'test/spec/**/*.js': ['browserify']
+      'test/**/*.js': ['browserify']
     },
 
     browserify: {
